17ebff2e254c306082c1fa73e77fba4fe257f2d4
Eric Berdondini <eric.pub@goth.it>
2011-08-08 13:03:40 +0200

    Fix for $base_url (#1217586), thanks to lazysoundsystem

--- cron.php
+++ cron.php
@@ -10,6 +10,10 @@ if (!file_exists('includes/bootstrap.inc') && preg_match('@^(.*)[\\\\/]sites[\\\
 
 include_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+global $base_url;
+$base_url = str_replace('/'.drupal_get_path('module', 'elysia_cron'), '', $base_url);
+
 if (function_exists('elysia_cron_run'))
   elysia_cron_run();
 else
