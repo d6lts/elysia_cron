3f435cf0b580071e6cf903223b71507b3f7ca0a7
Eric Berdondini <eric.pub@goth.it>
2011-10-06 00:46:45 +0200

    Issue #1239320 by Dane Powell (modified): Code Cleanup

--- cron.php
+++ cron.php
@@ -5,12 +5,19 @@
  * Handles incoming requests to fire off regularly-scheduled tasks (cron jobs).
  */
 
-if (!file_exists('includes/bootstrap.inc') && preg_match('@^(.*)[\\\\/]sites[\\\\/][^\\\\/]+[\\\\/]modules[\\\\/]([^\\\\/]+[\\\\/])?elysia(_cron)?$@', getcwd(), $r))
+if (!file_exists('includes/bootstrap.inc') && preg_match('@^(.*)[\\\\/]sites[\\\\/][^\\\\/]+[\\\\/]modules[\\\\/]([^\\\\/]+[\\\\/])?elysia(_cron)?$@', getcwd(), $r)) {
   chdir($r[1]);
+}
 
 include_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-if (function_exists('elysia_cron_run'))
+
+global $base_url;
+$base_url = str_replace('/' . drupal_get_path('module', 'elysia_cron'), '', $base_url);
+
+if (function_exists('elysia_cron_run')) {
   elysia_cron_run();
-else
+}
+else {
   drupal_cron_run();
+}
