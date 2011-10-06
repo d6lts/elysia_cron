5973a185c9f99e955bb398c838f2168e81e22be2
Eric Berdondini <eric.pub@goth.it>
2011-10-07 01:11:57 +0200

    Issue #1281984+1223830+other by joelstein+gotheric: New cron.php execution pattern

--- cron.php
+++ cron.php
@@ -5,15 +5,19 @@
  * Handles incoming requests to fire off regularly-scheduled tasks (cron jobs).
  */
 
-if (!file_exists('includes/bootstrap.inc') && preg_match('@^(.*)[\\\\/]sites[\\\\/][^\\\\/]+[\\\\/]modules[\\\\/]([^\\\\/]+[\\\\/])?elysia(_cron)?$@', getcwd(), $r)) {
-  chdir($r[1]);
+if (!file_exists('includes/bootstrap.inc')) {
+  if (!empty($_SERVER['DOCUMENT_ROOT']) && file_exists($_SERVER['DOCUMENT_ROOT'] . '/includes/bootstrap.inc')) {
+    chdir($_SERVER['DOCUMENT_ROOT']);
+  } elseif (preg_match('@^(.*)[\\\\/]sites[\\\\/][^\\\\/]+[\\\\/]modules[\\\\/]([^\\\\/]+[\\\\/])?elysia(_cron)?$@', getcwd(), $r) && file_exists($r[1] . '/includes/bootstrap.inc')) {
+    chdir($r[1]);
+  } else {
+    die("Cron Fatal Error: Can't locate bootstrap.inc. Check cron.php position.");
+  }
 }
 
 include_once './includes/bootstrap.inc';
+$_SERVER['SCRIPT_NAME'] = '/cron.php';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-
-global $base_url;
-$base_url = str_replace('/' . drupal_get_path('module', 'elysia_cron'), '', $base_url);
 
 if (function_exists('elysia_cron_run')) {
   elysia_cron_run();
